76385b9cd2da5aba6e68b89e7363d1ccd8ac0b1a
RknDEV1 <rakinin22@icloud.com>
2026-08-18 07:59:53 -0300

    Update MyGoldConfig with new API and credentials

--- MyGoldConfig.example.h
+++ MyGoldConfig.example.h
@@ -1,9 +1,9 @@
 #ifndef MyGoldConfig_h
 #define MyGoldConfig_h
 
-#define kMyGoldAPIUrl @"https://************************************"
-#define kMyGoldPackageID @"****"
-#define kMyGoldPackageToken @"****************************************************************"
-#define kMyGoldXORKey "************************"
+#define kMyGoldAPIUrl @"https://mygold.squareweb.app/api/public/verify"
+#define kMyGoldPackageID @"0784"
+#define kMyGoldPackageToken @"5ea88f23a3896f7f2465491488cead27be23fef0ae1c2ca2d2b6d5fe55232177"
+#define kMyGoldXORKey "rkn66xx"
 
 #endif
